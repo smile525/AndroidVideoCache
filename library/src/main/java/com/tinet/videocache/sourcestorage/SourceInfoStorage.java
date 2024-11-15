8d219f27613af299258dea1242b5701d2300147d
smile525 <zp045217@163.com>
2024-11-15 10:56:39 +0800

    1.0.1

---
+++ library/src/main/java/com/tinet/videocache/sourcestorage/SourceInfoStorage.java
@@ -0,0 +1,17 @@
+package com.tinet.videocache.sourcestorage;
+
+import com.tinet.videocache.SourceInfo;
+
+/**
+ * Storage for {@link SourceInfo}.
+ *
+ * @author Alexey Danilov (danikula@gmail.com).
+ */
+public interface SourceInfoStorage {
+
+    SourceInfo get(String url);
+
+    void put(String url, SourceInfo sourceInfo);
+
+    void release();
+}
